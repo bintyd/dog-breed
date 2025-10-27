8735bb79580ae8d224e255155479e569cb1beb50
bintydiaby <binty200625@gmail.com>
2025-10-26 20:38:19 -0400

    implimented main and caching breed fetcher

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,19 +13,22 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private int callsMade = 0;
+    private Map<String, List<String>> cache = new HashMap<>();
+    private BreedFetcher fetcher;
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+        this.fetcher = fetcher;
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
-    }
+        if (!cache.containsKey(breed)) {
+            cache.put(breed, this.fetcher.getSubBreeds(breed));
+        }
+        callsMade++;
+        return cache.get(breed);
+        }
 
     public int getCallsMade() {
-        return callsMade;
-    }
-}
+            return callsMade;
+        }}
--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -17,6 +17,10 @@ import java.util.*;
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
 
+    Request request = new Request.Builder()
+            .url("https://dog.ceo/api/breeds/list/all")
+            .build();
+
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
      * @param breed the breed to fetch sub breeds for
@@ -24,12 +28,23 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed)  {
+
+        try {
+            Response response = client.newCall(request).execute();
+            String jsonstring = response.body().string();
+            JSONObject jsonObject = new JSONObject(jsonstring);
+            ArrayList<String> breeds = new ArrayList<>();
+            JSONObject json_breeds = new JSONObject(jsonObject.get("message"));
+            JSONArray subbreeds = json_breeds.getJSONArray(breed);
+            for (int i = 0; i <= subbreeds.length(); i++) {
+                breeds.add(subbreeds.getString(i));
+            }
+            return breeds;
+
+        } catch (IOException e) {
+            throw new BreedNotFoundException(breed);
+
+        }
     }
 }
